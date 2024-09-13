2de5719fe08f447dca05d3e7d8fba1875d78023a
Murillo Ferraz <marteldelfer@gmail.com>
2024-09-13 19:27:23 -0300

    move to the right

--- 2048.js
+++ 2048.js
@@ -6,7 +6,7 @@ let grid = [
 ];
 
 function addNumber(grid) {
-    // first, count the 0
+    // first, count the 0s
     let count = 0;
     for (let i = 0; i < 16; i++) {
         if (grid[Math.floor(i/4)][i%4] == 0) {
@@ -26,6 +26,36 @@ function addNumber(grid) {
         }
     }
 }
+
+function update(grid) {
+
+    let id = ''
+    for (let i = 0; i < 16; i++) {
+        id = `${Math.floor(i/4) + 1}-${i%4 + 1}`;
+        let square = document.getElementById(id);
+        square.innerText = grid[Math.floor(i/4)][i%4];
+    }
+}
+
+function move(grid) {
+
+    let j;
+    // first, to the right
+    for (let i = 0; i < 15; i++) {
+        j = i + 1
+        // try to combine
+        if (grid[Math.floor(i/4)][i%4] == grid[Math.floor(j/4)][j%4] && !Number.isInteger((j/4))) {
+            grid[Math.floor(i/4)][i%4] = 0;
+            grid[Math.floor(j/4)][j%4] *= 2;
+        } else if (grid[Math.floor(j/4)][j%4] == 0 && !Number.isInteger((j/4))) {
+            //try to move
+            grid[Math.floor(j/4)][j%4] = grid[Math.floor(i/4)][i%4];
+            grid[Math.floor(i/4)][i%4] = 0;
+        }
+
+    }
+}
+
 document.addEventListener('keydown', function(event) {
     if (event.defaultPrevented) {
         return;
@@ -33,8 +63,8 @@ document.addEventListener('keydown', function(event) {
 
     switch (event.key) {
         case "ArrowDown":
-            addNumber(grid)
-            alert(grid)
+            addNumber(grid);
+            update(grid);
             break;
         case "ArrowUp":
             // TODO up()
@@ -43,7 +73,9 @@ document.addEventListener('keydown', function(event) {
         case "ArrowLeft":
             break;
         case "ArrowRight":
-            // TODO right()
+            move(grid);
+            addNumber(grid)
+            update(grid);
             break;
         default:
             return;
